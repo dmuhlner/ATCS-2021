debfd666c7a0b705b9b59bd1c4ed3f8fdadcecb4
Dakin Muhlner <dmuhlner@me.com>
2021-10-05 14:46:22 -0700

    Variables, Strings, and Numbers Exercises

---
+++ project0/firstName.py
@@ -0,0 +1,5 @@
+name = "dakin"
+
+print(name)
+print(name.title())
+print(name.upper())
---
+++ project0/oneTwo.py
@@ -0,0 +1,4 @@
+message = "Writing code in python"
+print(message)
+message = "That was printed using python code"
+print(message)
