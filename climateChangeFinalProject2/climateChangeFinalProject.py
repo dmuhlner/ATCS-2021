f0aa7d65471439f2c73e94f3e7694a0d42467dfa
Dakin Muhlner <dmuhlner@me.com>
2022-04-29 09:55:55 -0700

    Aggregate data

--- climateChangeFinalProject2/climateChangeFinalProject.py
+++ climateChangeFinalProject2/climateChangeFinalProject.py
@@ -4,22 +4,39 @@ import matplotlib.pyplot as plt
 import netCDF4 as nc
 import xarray as xr
 import geopy.distance
+import dask
 
 #data = pd.read_csv("crop yield data/Attainable yields (Mueller et al. 2012).csv")
 # ds = xr.open_dataset("crop yield data/maize/yield_1981.nc4")
 # dataframe = ds.to_dataframe()
 # print(dataframe.shape)
 
+def findTime(filename):
+    year = filename[-8:-4]
+    print(filename)
+    return int(year)
 
-data = xr.open_dataarray("gdhy_v1.2_v1.3_20190128/maize/yield_1993.nc4")
 
-print(data.values)
+def preProcess(ds):
+    filename = ds.encoding["source"]
+    time = findTime(filename)
+    data = ds.assign_coords({"year": time})
+    print(data)
+    return data
 
+def combineMFFolder(folder):
+    return xr.open_mfdataset((folder + "*.nc4"), preprocess=preProcess, combine="nested", concat_dim = "year")
+
+
+
+# xr.Dataset.to_netcdf(combineMFFolder("gdhy_v1.2_v1.3_20190128/wheat/"), "gdhy_v1.2_v1.3_20190128/aggregatedWheat.nc4")
+#
+# print(xr.open_dataset("gdhy_v1.2_v1.3_20190128/aggregatedWheat.nc4"))
 
 #def fillArrays():
 
-
-data = xr.Dataset(coords = {"year": range(1981, 2016)})
+#
+# data = xr.Dataset(coords = {"year": range(1981, 2016)})
 
 #
 # yields = data["var"][:]
